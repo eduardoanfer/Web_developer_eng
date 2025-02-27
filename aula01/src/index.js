c07cee727f4048bb28e3133d61dc4d8a05ad151f
Eduardo Andrade Ferreira <135661746+WizEDPill@users.noreply.github.com>
2025-02-27 16:01:23 -0300

    AT-02

---
+++ aula01/src/index.js
@@ -0,0 +1,25 @@
+const player01= {
+    nome="Mario",
+    velocidade=4,
+    manobrabilidade=3,
+    poder=3,
+};
+
+const player02= {
+    nome="Luigi",
+    velocidade=3,
+    manobrabilidade=4,
+    poder=4,
+};
+
+const player03= {
+    nome="Peach",
+    velocidade=2,
+    manobrabilidade=2,
+    poder=4,
+}
+
+const player04= {
+    nome="Toad",
+    
+}
